bf4adcb53421c8dd6e93de167d3192c82342ebe1
Prince Bablu <118957775+dev-programm@users.noreply.github.com>
2023-11-12 19:02:38 +0600

    I have done the mastering and server

--- WebCoderITLaravel-Project1/routes/web.php
+++ WebCoderITLaravel-Project1/routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", [frontendcontroller::class, "index"]);
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
